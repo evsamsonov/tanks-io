d082518391603a62f91a9550017d4326cea7700b
Alex Osipov <alex.osipov@tatar.ru>
2020-06-09 20:21:37 +0300

    Тест событий newPlayer, currentPlayers

--- assets/js/test-scene.js
+++ assets/js/test-scene.js
@@ -21,14 +21,24 @@ class TestScene extends Phaser.Scene
             console.log(arguments);
         });
 
-        this.socket.on('connect_timeout', function () {
-            console.log(arguments);
+        this.socket.on('newPlayer', (playerInfo) => {
+            console.log(playerInfo);
         });
 
-        this.socket.on('disconnect', () => {
-            console.log(arguments);
-        })
+        this.socket.on('currentPlayers', (players) => {
+            console.log(players);
+
+            Object.keys(players).forEach((id) => {
+                if (players[id].hasOwnProperty('playerId') && players[id].playerId === this.socket.id) {
+                    this.addPlayer(players[id]);
+                }
+            });
+        });
     }
 
     update() {}
+
+    addPlayer(playerInfo) {
+        this.tank = this.add.image(playerInfo.x, playerInfo.y, 'tank').setOrigin(0.5, 0.5);
+    }
 }
